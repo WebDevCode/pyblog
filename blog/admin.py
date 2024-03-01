2b4f2fd5b2db104866c8a680a5e3929c5d3f3d86
Tinu George <tinu.george@outlook.com>
2024-03-01 22:58:42 +0800

    add list and detail view, seo clean urls, and styles

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ["title", "slug", "author", "publish", "status"] #display on the Posts page
+  list_filter = ["status", "created", "publish", "author"]
+  search_fields = ["title", "body"]
+  prepopulated_fields = {"slug": ["title"]} # a dict with fields mapped to a list of fields
+  raw_id_fields = ["author"]
+  date_hierarchy = "publish"
+  ordering = ["status", "publish"]
+
+admin.site.register(Post, PostAdmin)
