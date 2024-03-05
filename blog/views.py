1eccdde33d7a075b6753cd4249951b6f43bf9f04
Tinu George <tinu.george@outlook.com>
2024-03-05 13:48:45 +0800

    add pagination and class based view

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,43 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DateDetailView
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Alternative class based Post List View
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
+
+class PostDetailView(DateDetailView):
+    queryset = Post.published.all()
+    template_name = "blog/post/detail.html"
+    month_format="%m"
+    slug_field = "slug" 
+    slug_url_kwarg="post"
+    date_field="publish"
+    
+    
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day, slug=post)
